597d2422c9984c39881e794c1c6c9c9b019406bb
Alexander <coolweak2@gmail.com>
2019-02-18 19:55:15 +0300

    Update WordsManipulation.cs

--- WordsManipulation.cs
+++ WordsManipulation.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text;
+using System.Linq;
 using System;
 
 namespace PadawansTask8
@@ -7,8 +8,52 @@ namespace PadawansTask8
     {
         public static void RemoveDuplicateWords(ref string text)
         {
-            // put your code here
-            throw new NotImplementedException();
+            if (text == null)
+            {
+                throw new ArgumentNullException();
+            }
+            if (text.Length == 0)
+            {
+                throw new ArgumentException();
+            }
+
+            char[] separators = new char[] { '.', ',', '!', '?', '-', ':', ';', ' ' };
+            string[] array = text.Split(separators);
+            int idx = 0;
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i] == " ")
+                {
+                    continue;
+                }
+                else
+                {
+                    for (int j = i + 1; j < array.Length; j++)
+                    {
+                        if (array[i].Equals(array[j]))
+                        {
+                            array[j] = " ";
+                        }
+                    }
+                    for (int x = 0; x < array.Length; x++)
+                    {
+                        if (array[x] != " ")
+                        {
+                            idx = x;
+                        }
+                    }
+                    if (i < idx)
+                    {
+                        array[i] += " ";
+                    }
+                }
+            }
+            text = null;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                text += array[i];
+            }
         }
     }
 }
